221c0e0bc94b173402c63b3a39cf18c3bf05e25e
Adryan Hawkins <teamzaal@gmail.com>
2020-01-15 02:40:31 +0000

    Remove files

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,15 @@ class User < ApplicationRecord
         :confirmable, :lockable, :timeoutable,
         :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2, :twitter]
 
-  def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
-      user.email = provider_data.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.skip_confirmation!
-    end
-  end
 
   has_many :games
   has_many :pieces
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
 
 end
 
