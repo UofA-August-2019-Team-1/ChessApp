c862fcd2f853da7928008296257123ff9fae445e
Jayson Coppo <jayson.coppo@neudesic.com>
2020-01-19 09:22:33 -0700

    draw pieces from db

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,21 +6,34 @@ class Game < ApplicationRecord
 
   has_many :pieces
   # has_many :user_games
+  after_create :set_up_board
 
-  attr_accessor :board
+  def set_up_board
+    # WHITE PIECES
+      # Pawns
+      (0..7).each do |x|
+        Pawn.create(x_position: x, y_position: 6, user_id: white_player_id, game_id: id, color: 'white')
+      end
 
-  # def initialize()
-  #   @board = [
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #     [0, 0, 1, 0, 0, 0, 0, 0],
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #     [0, 0, 0, 0, 0, 0, 1, 0],
-  #     [0, 0, 0, 0, 0, 0, 0, 0],
-  #   ]
-  # end
+      # Rooks
+      [0, 7].each do |x|
+        Rook.create(x_position: x, y_position: 7, user_id: white_player_id, game_id: id, color: 'white')
+      end
 
+      # Knights
+      [1, 6].each do |x|
+        Knight.create(x_position: x, y_position: 7, user_id: white_player_id, game_id: id, color: 'white')
+      end
+
+      # Bishops
+      [2, 5].each do |x|
+        Bishop.create(x_position: x, y_position: 7, user_id: white_player_id, game_id: id, color: 'white')
+      end
+
+      # Queen
+      Queen.create(x_position: 3, y_position: 7, user_id: white_player_id, game_id: id, color: 'white')
+
+      #King
+      King.create(x_position: 4, y_position: 7, user_id: white_player_id, game_id: id, color: 'white')
+  end
 end
