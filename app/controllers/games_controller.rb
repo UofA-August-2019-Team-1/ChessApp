7f0d4da24d88933c616d975c656dc12725b69091
Jayson Coppo <jayson.coppo@neudesic.com>
2020-01-22 23:21:09 -0700

    join works
    bug: when not signed in, available games is not populated, user can't see them

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,11 @@ class GamesController < ApplicationController
   before_action :verify_different_user, only:[:join]
 
     def index
-      @games_available = Game.where(:white_player_id => nil).where.not(:black_player_id => nil).or (Game.where.not(:white_player_id => nil).where(:black_player_id => nil))
+
+      if current_user
+        @games_available = Game.where.not(:black_player_id => current_user.id).or (Game.where.not(:white_player_id => current_user.id))
+        @games_active = Game.where(:white_player_id => current_user.id).or(Game.where(:black_player_id => current_user.id))
+      end
     end
 
     def new
@@ -15,7 +19,7 @@ class GamesController < ApplicationController
       new_game_setup_ids
 
       if @game.valid?
-        redirect_to root_path
+        redirect_to game_path(@game)
       else
         render :new, status: :unprocessable_entity
       end
@@ -27,23 +31,16 @@ class GamesController < ApplicationController
     end
 
     def update
-    end
-
-    def join
-      @game = Game.find_by_id(params[:id])
-      @pieces = @game.pieces
-      @pieces.where(user_id:nil).update_all(user_id: current_user.id)
-
-      @game.update_attributes(game_params)
-      @game.users << current_user
-      @piece = Piece.find(params[:id])
+      @game = Game.find(params[:id])
+      @game.update_attributes(black_player_id: current_user.id)
+      join_game_setup_ids
       redirect_to game_path(@game)
     end
 
     def destroy
       @game = Game.find_by_id(params[:id])
       @game.pieces.destroy_all
-      @games.destroy
+      @game.destroy
       redirect_to games_path
     end
 
@@ -55,5 +52,9 @@ class GamesController < ApplicationController
 
     def new_game_setup_ids
       @game.pieces.where(color: 'white').update_all(user_id: @game.white_player_id)
+    end
+
+    def join_game_setup_ids
+      @game.pieces.where(color: 'black').update_all(user_id: @game.black_player_id)
     end
 end
