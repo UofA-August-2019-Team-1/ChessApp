da0537077c40f424a7ddca48f0233d6f079552ea
Jayson Coppo <jayson.coppo@neudesic.com>
2020-02-05 20:35:26 -0700

    player can forfeit
    games table now 'status' and 'winner_id' attributes
    end game modal

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "games#index"
-  resources :games
+  resources :games do
+  	member do
+  		post :forfeit_game
+  	end
+  end
   resources :pieces
   resources :users
 end
